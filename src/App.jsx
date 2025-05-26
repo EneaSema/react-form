73ad17b314bf827737eccdf2297c91b84db03789
EneaSema <sema.enea1993@gmail.com>
2025-05-26 15:05:17 +0200

    resolve-first-question

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 function App() {
+  const articols = [`bicchieri`, `posate`, `pentolame`];
+  console.log(articols);
+
   return (
     <>
       <main>
-        <h1>Ciao</h1>
+        <h1>Lista articoli</h1>
+        <ul>
+          <li>{articols[0]}</li>
+          <li>{articols[1]}</li>
+          <li>{articols[2]}</li>
+        </ul>
       </main>
     </>
   );
