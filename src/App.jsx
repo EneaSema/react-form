8696e805c288a0f959b50d1546a81de11f9141bd
EneaSema <sema.enea1993@gmail.com>
2025-05-26 16:04:10 +0200

    problem-situation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
+
 function App() {
   const articols = [`bicchieri`, `posate`, `pentolame`];
-  console.log(articols);
+
+  const [articol, setArticol] = useState(articols);
+  console.log(articol);
+
+  const newArticols = [...articols];
+
+  const [newArticol, setNewArticol] = useState("");
 
   return (
     <>
       <main>
-        <h1>Lista articoli</h1>
-        <ul>
-          <li>{articols[0]}</li>
-          <li>{articols[1]}</li>
-          <li>{articols[2]}</li>
-        </ul>
+        <div className="container">
+          <h1>Lista articoli</h1>
+          <ul>
+            {articols.map((articol, index) => (
+              <li onChange={setArticol} key={index}>
+                {articol}
+              </li>
+            ))}
+          </ul>
+
+          <form className="form-add-articol">
+            <input onChange={setNewArticol} type="text" value={newArticol} />
+            <input className="btn-form-add" type="submit" value="Aggiungi" />
+          </form>
+        </div>
       </main>
     </>
   );
